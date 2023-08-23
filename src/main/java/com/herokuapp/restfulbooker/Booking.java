2c6576d64af8f575852833dc1c2f4fb74c2b9d9b
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-08-23 19:45:02 +0200

    Pojo objects

---
+++ src/main/java/com/herokuapp/restfulbooker/Booking.java
@@ -0,0 +1,27 @@
+package com.herokuapp.restfulbooker;
+
+public class Booking {
+    String firstname;
+    String lastname;
+    int totalprice;
+    boolean depositpaid;
+    String additionalneeds;
+
+
+
+
+
+    /*
+    '{
+    "firstname" : "Jim",
+    "lastname" : "Brown",
+    "totalprice" : 111,
+    "depositpaid" : true,
+    "bookingdates" : {
+        "checkin" : "2018-01-01",
+        "checkout" : "2019-01-01"
+    },
+    "additionalneeds" : "Breakfast"
+}' */
+
+}
---
+++ src/main/java/com/herokuapp/restfulbooker/Bookingdates.java
@@ -0,0 +1,8 @@
+package com.herokuapp.restfulbooker;
+
+public class Bookingdates {
+
+    private String checkin;
+    private String heckout;
+    
+}
