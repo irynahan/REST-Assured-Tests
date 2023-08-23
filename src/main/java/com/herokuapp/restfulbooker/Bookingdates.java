7d99a903f3f4d2e5a5eb8ec2bf7a7e0e1e743726
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-08-23 19:59:57 +0200

    Pojo objects setters getters

--- src/main/java/com/herokuapp/restfulbooker/Bookingdates.java
+++ src/main/java/com/herokuapp/restfulbooker/Bookingdates.java
@@ -4,5 +4,33 @@ public class Bookingdates {
 
     private String checkin;
     private String heckout;
-    
+
+    public Bookingdates(String checkin, String heckout) {
+        this.checkin = checkin;
+        this.heckout = heckout;
+    }
+
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getHeckout() {
+        return heckout;
+    }
+
+    public void setHeckout(String heckout) {
+        this.heckout = heckout;
+    }
+
+    @Override
+    public String toString() {
+        return "Bookingdates{" +
+                "checkin='" + checkin + '\'' +
+                ", heckout='" + heckout + '\'' +
+                '}';
+    }
 }
